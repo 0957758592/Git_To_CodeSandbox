ba1284454176a93dd88f51ccba811e9123af6fe6
Roman Danilov <r.danylov@gmail.com>
2019-06-23 02:56:11 +0300

    added listeners without reloading page

--- src.js
+++ src.js
@@ -1,16 +1,39 @@
 ﻿	"use strict";
-	let newURL = '';
-	let json = '';
+	const GIT = "https://github.com/"
 
-	chrome.runtime.onMessage.addListener(function(urls) {
-		if(urls.data){
-			newURL = urls.data[0];
-			json = urls.data[1];
-		}
+	chrome.tabs.onUpdated.addListener(
+	  function(tabId, changeInfo, tab) {
+	    if (changeInfo.url) {
+	    	useListener(tabId);
+	    }
+	  }
+	);
+
+	chrome.tabs.onActivated.addListener(function(activeInfo) {
+		useListener(activeInfo.tabId);
+	}); 
+
+	chrome.browserAction.onClicked.addListener(function(tabs) {  
+		chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+		chrome.tabs.executeScript(tabs[0].id, {code: "var json = document.querySelector('[title=\"package.json\"]') !== null; json"}, function(res){
+				res[0] ? 
+				confirm("Would you like to execute this code?") ? 
+					window.open("" + `https://codesandbox.io/s/github/${tabs[0].url.replace(GIT, "")}`) : 
+					false : 
+				alert("File 'package.json' is not exist at this page");
+			});
+		});
 	});
 
-	chrome.browserAction.onClicked.addListener(function(tab) {  
-		json ? confirm("Would you like to execute this code?") ? window.open("" + `https://codesandbox.io/s/github${newURL}`) : false : alert("File 'package.json' is not exist at this page");
-	});
+	function getStatus(status) {
+		chrome.browserAction.setBadgeText({text: status ? "on" : "off"}, null);
+		chrome.browserAction.setBadgeBackgroundColor({color: status ? [0, 180, 0, 100] : [180, 0, 0, 100]}, null);
+	}
 
-
+	function useListener(tabId) {
+		chrome.tabs.get(tabId, function(tab){
+	  		tab.url.includes(GIT) ? 
+	  			chrome.browserAction.enable(tab.id, getStatus(true)) : 
+	  			chrome.browserAction.disable(tab.id, getStatus(false));
+	  	});
+	}
